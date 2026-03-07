55a3657cc8b4fb63b53e2625366b46880e1192b4
Xyp1ce <ramdev4559@gmail.com>
2026-03-06 16:09:42 -0800

    Singleton Pattern Finished
    Se terminó la implementación del patron singlenton

---
+++ Singleton/ChocolateBoiler.java
@@ -0,0 +1,43 @@
+public class ChocolateBoiler {
+  private boolean empty;
+  private boolean boiled;
+
+  // variable para verificar singleton
+  public static ChocolateBoiler uniqueInstance = new ChocolateBoiler();
+
+  private ChocolateBoiler() {
+    empty = true;
+    boiled = false;
+  }
+
+  public static ChocolateBoiler getInstance() {
+    return uniqueInstance;
+  }
+
+  public void fill() {
+    if (isEmpty()) {
+      empty = false;
+      boiled = false;
+    }
+  }
+
+  public void drain() {
+    if (!isEmpty() && isBoiled()) {
+      empty = true;
+    }
+  }
+
+  public void boil() {
+    if (!isEmpty() && isBoiled()) {
+      empty = true;
+    }
+  }
+
+  public boolean isEmpty() {
+    return empty;
+  }
+
+  public boolean isBoiled() {
+    return boiled;
+  }
+}
